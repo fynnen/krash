ff706f76a2e9ed39fe709213c364a7313f2f156a
Francis Lapierre <francis.lapierre@radio-canada.ca>
2019-04-24 14:50:18 -0400

    gestion du state et structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, useState, useRef } from 'react';
 import './App.css';
 
 class App extends Component {
@@ -15,3 +14,63 @@ class App extends Component {
 }
 
 export default App;
+
+
+const getTeams = (participants, sortMethod) => {
+
+  return [];
+}
+
+const Krash = () => {
+  const [participants, setParticipants] = useState([]);
+  const [sortMethod, setSortMethod] = useState(null);
+  const teams = getTeams(participants, sortMethod);
+  const saveParticipants = (participants, sortMethod) => {
+    setParticipants(participants);
+    setSortMethod(sortMethod);
+  }
+  return (
+    <div>
+      <ParticipantsInput save={saveParticipants} />
+      <TeamsPanel teams={teams} />
+    </div>
+  )
+}
+
+const ParticipantsInput = (props) => {
+  const { save } = props;
+  const participantsRef = useRef(null);
+  const sortMethodRef = useRef(null);
+  const participants = null; //remplacer par les valeurs de ref
+  const sortMethod = null; //remplacer par les valeurs de ref
+  return (
+    <>
+      <textarea
+        id="participants"
+        ref={participantsRef}
+        placeholder="Liste des participants"
+      />
+      <select ref={sortMethodRef}>
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+      <button
+        onClick={() => save(participants, sortMethod)}
+        value="Sauvegarder"
+      >
+        Sauvegarder
+      </button>
+    </>
+  );
+}
+
+const TeamsPanel = (props) => {
+  const { teams } = props;
+  return(
+    <div>
+      Placeholder pour remz le meilleur
+    </div>
+  )
+}
