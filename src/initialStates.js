61be3fb58fcdb4b9df71145b36356bb6c5b093d6
Sebastien Lepine <sebastien.lepine@radio-canada.ca>
2019-05-06 11:47:02 -0400

    Remplir la liste des participants avec la prod existante

--- src/initialStates.js
+++ src/initialStates.js
@@ -1,8 +1,8 @@
 export const initialParticipants = [
   {
     id : 1,
-    name: "Oli St-Louis",
-    role: "QA",
+    name: "Olivier St-Louis-Desjardins",
+    role: "BE",
     location: "MTL",
     available: true
   },
@@ -15,16 +15,198 @@ export const initialParticipants = [
   },
   {
     id : 3,
-    name: "Sébastien Lepine",
+    name: "Sébastien Lépine",
     role: "FE",
-    location: "HOME",
+    location: "MTL",
     available: true
   },
   {
     id: 4,
     name: "Francis Lapierre",
     role: "BE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 5,
+    name: "Alexandre Daignault",
+    role: "BE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 6,
+    name: "Philippe Lamarre",
+    role: "FE",
     location: "QC",
+    available: true
+  },
+  {
+    id: 7,
+    name: "Benoit Ducharme",
+    role: "FE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 8,
+    name: "Nicolas Asselin",
+    role: "BE",
+    location: "QC",
+    available: true
+  },
+  {
+    id: 9,
+    name: "Mathieu Corriveau",
+    role: "FE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 10,
+    name: "Cédric Edouard",
+    role: "FE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 11,
+    name: "Dominic Gauthier",
+    role: "BE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 11,
+    name: "Frédéric Croix",
+    role: "FE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 12,
+    name: "Charles-Jérémy Colnet",
+    role: "BE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 13,
+    name: "Frédérik Charest",
+    role: "BE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 14,
+    name: "Xavier Vincent",
+    role: "BE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 15,
+    name: "Jacob Tremblay",
+    role: "BE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 16,
+    name: "Véronique Leclerc",
+    role: "FE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 17,
+    name: "Marc Bertrand",
+    role: "BE",
+    location: "QC",
+    available: true
+  },
+  {
+    id: 18,
+    name: "Patrice Gamelin",
+    role: "BE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 19,
+    name: "Marie-Ève Bournival",
+    role: "FE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 20,
+    name: "Olivier Fortin",
+    role: "FE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 21,
+    name: "Martin Marquis",
+    role: "BE",
+    location: "QC",
+    available: true
+  },
+  {
+    id: 22,
+    name: "Mykael Adam",
+    role: "FE",
+    location: "QC",
+    available: true
+  },
+  {
+    id: 23,
+    name: "Frédéric Landry",
+    role: "FE",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 24,
+    name: "David Larose",
+    role: "QA",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 25,
+    name: "David Larose",
+    role: "QA",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 26,
+    name: "Victor Marchand",
+    role: "QA",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 27,
+    name: "Kathleen Finnegan",
+    role: "QA",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 28,
+    name: "Sofiane Benmila",
+    role: "QA",
+    location: "MTL",
+    available: true
+  },
+  {
+    id: 29,
+    name: "Jean-François David",
+    role: "QA",
+    location: "MTL",
     available: true
   }
 ];
