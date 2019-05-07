3f53e7d0601b90893e9df83cc41f189dca314cde
Fynnen <lapierre.francis@gmail.com>
2019-05-06 22:07:03 -0400

    add eslint and prettier

--- src/constants.js
+++ src/constants.js
@@ -1,36 +1,36 @@
 export const SORTMETHODS = {
-  Random: "Random",
-  Mixed: "Mixed",
-  Splitted: "Splitted"
+  Random: 'Random',
+  Mixed: 'Mixed',
+  Splitted: 'Splitted'
 };
 
 export const LOCATIONS = {
-  MTL: {id: "mtl", name: "Montréal"},
-  QC: {id: "qc", name: "Québec"},
-  HOME: {id: "home", name: "Distance"},
+  MTL: { id: 'mtl', name: 'Montréal' },
+  QC: { id: 'qc', name: 'Québec' },
+  HOME: { id: 'home', name: 'Distance' }
 };
 
 export const ROLES = {
-  FE: {id: "fe", name: "Front-end"},
-  BE: {id: "be", name: "Back-end"},
-  QA: {id: "qa", name: "Qualité assurance"},
+  FE: { id: 'fe', name: 'Front-end' },
+  BE: { id: 'be', name: 'Back-end' },
+  QA: { id: 'qa', name: 'Qualité assurance' }
 };
 
 export const BREAKPOINTS = {
   XS: {
     min: 0,
-    max: 480,
+    max: 480
   },
   SM: {
     min: 481,
-    max: 720,
+    max: 720
   },
   MD: {
     min: 721,
-    max: 1024,
+    max: 1024
   },
   LG: {
     min: 1025,
-    max: 99999,
+    max: 99999
   }
 };
