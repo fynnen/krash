c9ad9439c79dc146d490621d487b2c549f25bf28
Sebastien Lepine <sebastien.lepine@radio-canada.ca>
2020-06-04 13:23:00 -0400

    padding header xs et sm

--- src/components/styled/HeaderStyled.js
+++ src/components/styled/HeaderStyled.js
@@ -1,16 +1,22 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { BREAKPOINTS as bp } from "../../constants";
+import { mediaMin } from  "../../helpers/mediaQuery";
 import { paletteColors } from "../../helpers/globalStyles";
 
 export const HeaderStyled = styled.header`
-  ${({ isNightMode }) => `
+  ${({ isNightMode }) => css`
     align-content: center;
     background: ${isNightMode ? paletteColors.primary : paletteColors.primary};
     display: flex;
     justify-content: space-between;
-		padding: 20px;
+		padding: 10px;
 		position: sticky;
 		top: 0;
 		z-index: 1;
+
+		${mediaMin(bp.SM.min, css`
+			padding: 20px;
+		`)}
 
     h1 {
       align-items: center;
