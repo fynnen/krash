5677eedc9d9223efc50142b69f97af180e99fb54
Samuel Melanson <samuel.melanson@radio-canada.ca>
2020-06-12 16:42:20 -0400

    toggle-text-align

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import HeaderStyled from './styled/HeaderStyled';
 import ToggleNightMode from './ToggleNightMode';
+import { IoIosArrowDropright, IoIosArrowDropdown } from 'react-icons/io';
 
 export const Header = (props) => {
   const {
@@ -10,13 +11,22 @@ export const Header = (props) => {
     toggleNightMode,
   } = props;
 
+  const [isVerticalText, setisVerticalText] = useState(false);
+
   return (
     <HeaderStyled
       className="App-header"
       isNightMode={isNightMode}
+      isVerticalText={isVerticalText}
       ref={headerRef}
     >
       <h1>KrAsH</h1>
+      <button className="toggle-text-align" onClick={()=>{setisVerticalText(!isVerticalText)}}>
+        {isVerticalText ?         
+          <IoIosArrowDropdown /> :
+          <IoIosArrowDropright/>
+        }        
+      </button>
       <ToggleNightMode
         isNightMode={isNightMode}
         toggleNightMode={toggleNightMode}
